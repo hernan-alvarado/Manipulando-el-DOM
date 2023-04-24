0964bc712e4f7708d244992373a458bf7f749ecb
hernnalvarado <hernnalvarado@gmail.com>
2023-04-23 23:59:39 -0400

    primer commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function mostrarMensaje(){ 
+    
+    document.getElementById('text2').style.display = "block"; 
+}
+
+function ocultarMensaje(){ 
+    
+    document.getElementById('text2').style.display = "none";
+}
+
